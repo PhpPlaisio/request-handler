ed894f0705347282e9769ecff02dc2592992f0be
Paul Water <p.r.water@setbased.nl>
2018-07-18 10:50:10 +0200

    Added method addListener.

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -9,12 +9,33 @@ interface RequestHandler
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * Adds a listener that must be notified when an event occurs.
+   *
+   * The following events must be implemented be a concrete class:
+   * <ul>
+   * <li> post_render: This event occurs after a page requested has been successfully handled.
+   * <li> post_commit: This event occurs after a page requested has been handled and the database transaction has been
+   *                   committed. The listener CAN NOT access the database or session data.
+   * </ul>
+   *
+   * @param string   $event    The name of the event.
+   * @param callable $listener The listener that must be notified when the event occurs.
+   *
+   * @return void
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function addListener(string $event, callable $listener): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * Returns the ID of the page currently requested.
    *
    * @return int|null
    *
    * @api
-   * @since 3.0.0
+   * @since 1.0.0
    */
   public function getPagId(): ?int;
 
